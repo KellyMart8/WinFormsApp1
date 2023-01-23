524cc41343e99e99009cc6e3066af61e3e599e97
JADPA-25 <JADPA-25@JADPA-25>
2023-01-23 11:54:34 -0600

    Agregar archivos de proyecto.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,64 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtInssL.Enabled = false;   //para no ingresar
+            txtInssP.Enabled = false;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double ingr, il, ip;
+            ingr = double.Parse(txtIngresos.Text);
+
+            if (ingr > 4800 && ingr <= 500000)
+            {
+                il = ingr * 0.07;
+                txtInssL.Text = il.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Rango entre 4800 y 500000", "ERROR");
+            }
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtIngresos.Text = "";
+            txtInssL.Text = "";
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtIngresos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar) && (e.KeyChar != (char)Keys.Back)))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void cboTipD_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
